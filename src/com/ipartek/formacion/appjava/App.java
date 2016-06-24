fa269b5e0e083ace67b48f3f656b8bb130368207
AndoniLuna <andoniluna22@hotmail.com>
2016-06-24 10:51:57 +0200

    This is the third modification

--- src/com/ipartek/formacion/appjava/App.java
+++ src/com/ipartek/formacion/appjava/App.java
@@ -4,7 +4,7 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Soy una app de java SE 7");
-		System.out.println("Segundo commit");
+		System.out.println("Esta es la 3ª modificación");
 	}
 
 }
